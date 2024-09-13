c98390ff099069513d3108808738b030faab22a1
DanikGuun <80112309+DanikGuun@users.noreply.github.com>
2024-09-13 10:44:45 +0300

    Сверстал верхнее меню

--- ToDo/ViewController.swift
+++ ToDo/ViewController.swift
@@ -6,14 +6,54 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setup()
     }
-
+    
+    private func setup(){
+        self.view.backgroundColor = .background
+        //Today Tasks Label
+        let todayTasksLabel = UILabel()
+        self.view.addSubview(todayTasksLabel)
+        todayTasksLabel.text = "Задачи на сегодня"
+        todayTasksLabel.font = UIFont.systemFont(ofSize: 22, weight: .semibold)
+        todayTasksLabel.snp.makeConstraints { maker in
+            maker.top.equalTo(view.safeAreaLayoutGuide)
+            maker.leading.equalTo(view.safeAreaLayoutGuide).inset(15)
+        }
+        //Today Date Label
+        let todayDateLabel = UILabel()
+        self.view.addSubview(todayDateLabel)
+        todayDateLabel.text = Date().formatted(.dateTime.weekday(.wide).day().month(.wide))
+        todayDateLabel.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        todayDateLabel.textColor = .secondaryLabel
+        todayDateLabel.snp.makeConstraints { maker in
+            maker.leading.equalTo(todayTasksLabel.snp.leading)
+            maker.top.equalTo(todayTasksLabel.snp.bottom).offset(5)
+        }
+        //Add Task Button
+        let addTaskButton = UIButton(configuration: .tinted())
+        self.view.addSubview(addTaskButton)
+        var conf = addTaskButton.configuration
+        var attributedTitle = AttributedString(stringLiteral: "Задача")
+        attributedTitle.font = UIFont.systemFont(ofSize: 14, weight: .semibold)
+        conf?.attributedTitle = attributedTitle
+        conf?.cornerStyle = .large
+        conf?.image = UIImage(systemName: "plus", withConfiguration: UIImage.SymbolConfiguration(pointSize: 14, weight: .semibold, scale: .medium))
+        conf?.imagePadding = 5
+        conf?.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20)
+        addTaskButton.configuration = conf
+        
+        addTaskButton.snp.makeConstraints { maker in
+            maker.trailing.equalToSuperview().inset(15)
+            maker.centerY.equalTo(todayTasksLabel.snp.bottom)
+        }
+    }
 
 }
 
